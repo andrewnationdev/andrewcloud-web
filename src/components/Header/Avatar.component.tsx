5d52d9b349fdbf736f0402bc517f99d3b3d77aa6
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-18 18:18:12 -0300

    alterando avatar

--- src/components/Header/Avatar.component.tsx
+++ src/components/Header/Avatar.component.tsx
@@ -1,6 +1,6 @@
 export default function AvatarComponent() {
     return (
-        <div class="chip">
+        <div className="chip">
             <img src="images/yuna.jpg" alt="Contact Person" />
             Jane Doe
         </div>
