790c503e5808e57aae488e08be1c9fd0203077d9
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-18 19:15:36 -0300

    ajuste div

--- src/components/Header/AppLogo.component.tsx
+++ src/components/Header/AppLogo.component.tsx
@@ -2,9 +2,9 @@ import '../../styles/header.css';
 
 export default function AppLogoComponent(){
     return(
-        <>
+        <div>
             <img className="app-logo" src="/assets/logo.png" alt="AndrewCloudlogo" />
             <span className="app-logo-text">AndrewCloud</span>
-        </>
+        </div>
     );
 }
