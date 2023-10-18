0a115cd3d675592c5ef633b6912955ec3cb07a4a
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-18 19:11:29 -0300

    adicionando medidor de armazenamento

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -4,6 +4,7 @@ export default function HomePage(){
     return(
         <>
             <HeaderComponent/>
+            <meter id="disk_c" value="3.11" min="0" max="100">3.11 GB de 100 GB usados</meter>
         </>
     );
 }
