6e43ec3ff7339cce77f431416ba419899af2de46
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-18 18:09:25 -0300

    adicionando avatar

--- src/components/Header/Avatar.component.tsx
+++ src/components/Header/Avatar.component.tsx
@@ -1,5 +1,8 @@
-export default function AvatarComponent(){
-    return(
-        <></>
+export default function AvatarComponent() {
+    return (
+        <div class="chip">
+            <img src="images/yuna.jpg" alt="Contact Person" />
+            Jane Doe
+        </div>
     );
 }
