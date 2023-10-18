ed66bb936a1eb4a8eaa3caf5fec41a30c721dbbd
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-18 19:10:35 -0300

    mudando nome avatar

--- src/components/Header/Avatar.component.tsx
+++ src/components/Header/Avatar.component.tsx
@@ -2,7 +2,7 @@ export default function AvatarComponent() {
     return (
         <div className="chip">
             <img src="https://th.bing.com/th/id/OIP._Ub60e6muH_f6fAyfyZW8AHaEK?pid=ImgDet&rs=1" alt="Contact Person" />
-            Jane Doe
+            GHT23
         </div>
     );
 }
