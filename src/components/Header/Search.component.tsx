9a4131da398040f3c8d67965804e3664f5c304c9
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-18 19:03:59 -0300

    barra de busca

--- src/components/Header/Search.component.tsx
+++ src/components/Header/Search.component.tsx
@@ -2,9 +2,9 @@ import '../../styles/header.css';
 
 export default function SearchComponent() {
     return (
-        <div className="row">
-            <form className="col">
-                <div className="row">
+        <div>
+            <form >
+                <div>
                     <div className="input-field col">
                         <i className="material-icons prefix">search</i>
                         <textarea className="materialize-textarea search-field"></textarea>
