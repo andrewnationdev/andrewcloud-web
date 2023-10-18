488f8600c1d11bba1fe62b5af20757cf1aac2deb
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-18 18:46:28 -0300

    ajuste barra de pesquisa

--- src/components/Header/Search.component.tsx
+++ src/components/Header/Search.component.tsx
@@ -1,11 +1,13 @@
+import '../../styles/header.css';
+
 export default function SearchComponent() {
     return (
         <div className="row">
-            <form className="col s12">
+            <form className="col">
                 <div className="row">
-                    <div className="input-field col s6">
+                    <div className="input-field col">
                         <i className="material-icons prefix">search</i>
-                        <textarea className="materialize-textarea"></textarea>
+                        <textarea className="materialize-textarea search-field"></textarea>
                     </div>
                 </div>
             </form>
