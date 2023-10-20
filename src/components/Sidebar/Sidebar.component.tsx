d017da81c668120dd4551609208cd1eeefe765be
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 10:07:21 -0300

    meter

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -3,7 +3,9 @@ import '../../styles/main.css';
 export default function SidebarComponent(){
     return(
         <div className="sidebar-container">
-            <meter min="0" max="100" value="75">75% Used</meter>
+            <meter min="0" max="100" value="98.1" high="10" low="90" optimum="50">98% Used</meter>
+            <span>98.1 GB de 100 GB usados</span>
+            <meter min="0" max="100" value="3.11" high="10" low="90" optimum="50">3% Used</meter>
             <span>3.11 GB de 100 GB usados</span>
         </div>
     );
