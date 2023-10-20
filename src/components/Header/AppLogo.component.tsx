a44ba0022511e8c4b92f40c14a7f60ff0d1641ba
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 09:41:00 -0300

    modificando header e adicionando efeitos

--- src/components/Header/AppLogo.component.tsx
+++ src/components/Header/AppLogo.component.tsx
@@ -2,7 +2,7 @@ import '../../styles/header.css';
 
 export default function AppLogoComponent(){
     return(
-        <div>
+        <div className="app-logo-container">
             <img className="app-logo" src="/assets/logo.png" alt="AndrewCloudlogo" />
             <span className="app-logo-text">AndrewCloud</span>
         </div>
--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -5,13 +5,19 @@ import SearchComponent from './Search.component';
 export default function HeaderComponent(){
     return(
         <header>
+            <div style={{
+                display: "flex",
+                gap: "1rem",
+                alignItems: "center"
+            }}>
             <AppLogoComponent/>
             <SearchComponent/>
+            </div>
             <div>
-                <a className="btn-floating btn-large waves-effect waves-light blue darken-2">
+                <a className="btn-floating btn-small waves-effect waves-light blue darken-2">
                     <i className="material-icons">add</i>
                 </a>
-                <a className="btn-floating btn-large waves-effect waves-light light-blue darken-1">
+                <a className="btn-floating btn-small waves-effect waves-light light-blue darken-1">
                     <i className="material-icons">settings</i>
                 </a>
             </div>
