022d2c6311d9809e117088a29162104036c65823
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 09:45:51 -0300

    ajuste header

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -2,26 +2,36 @@ import AvatarComponent from './Avatar.component';
 import AppLogoComponent from './AppLogo.component';
 import SearchComponent from './Search.component';
 
-export default function HeaderComponent(){
-    return(
+export default function HeaderComponent() {
+    return (
         <header>
             <div style={{
                 display: "flex",
                 gap: "1rem",
                 alignItems: "center"
             }}>
-            <AppLogoComponent/>
-            <SearchComponent/>
+                <AppLogoComponent />
+                <SearchComponent />
             </div>
-            <div>
-                <a className="btn-floating btn-small waves-effect waves-light blue darken-2">
-                    <i className="material-icons">add</i>
-                </a>
-                <a className="btn-floating btn-small waves-effect waves-light light-blue darken-1">
-                    <i className="material-icons">settings</i>
-                </a>
+            <div style={{
+                display: "flex",
+                gap: "1rem",
+                alignItems: "center"
+            }}>
+                <div style={{
+                    display: "flex",
+                    gap: "1rem",
+                    alignItems: "center"
+                }}>
+                    <a className="btn-floating waves-effect waves-light blue darken-2">
+                        <i className="material-icons">add</i>
+                    </a>
+                    <a className="btn-floating  waves-effect waves-light light-blue darken-1">
+                        <i className="material-icons">settings</i>
+                    </a>
+                </div>
+                <AvatarComponent />
             </div>
-            <AvatarComponent/>
         </header>
     );
 }
