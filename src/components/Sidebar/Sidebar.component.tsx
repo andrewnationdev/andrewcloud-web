3116cdcce2071d889346eb099856e94f480c2d5f
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 23:14:28 -0300

    correção

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -10,11 +10,13 @@ export default function SidebarComponent() {
                 Enviar arquivo
             </a>
             <input type="file" id="avatar" hidden name="avatar" accept="image/png, image/jpeg" />
-            
-             </span><strong>Tamanho:</strong>11MB<span>
-             </span><strong>Tipo:</strong> PNG<span>
-             </span><strong>Modificado em:</strong> 25/08/2023 às 19:19<span>
-             </span><strong>Acesso:</strong> Privado<span>
+            <hr/>
+            <strong>Informações do arquivo</strong>
+            <span><strong>Nome:</strong> nome.txt</span>
+             <span><strong>Tamanho:</strong>11MB</span>
+             <span><strong>Tipo:</strong> PNG</span>
+             <span><strong>Modificado em:</strong> 25/08/2023 às 19:19</span>
+             <span><strong>Acesso:</strong> Privado</span>
             
             <meter min="0" max="100" value="98.1" high={10} low={90} optimum={50}>98% Used</meter>
             <span>98.1 GB de 100 GB usados</span>
