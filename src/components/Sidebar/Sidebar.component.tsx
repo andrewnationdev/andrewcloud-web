daedd44266acac5e4c72be77b1911c3db70f0753
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 22:12:27 -0300

    adicionando botao

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -3,11 +3,11 @@ import '../../styles/main.css';
 export default function SidebarComponent(){
     return(
         <div className="sidebar-container">
-            {/* adicionar daqui pra baixo o botão de upload */}
+            <a className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">add</i></a>
             {/* Adicionar daqui pra baixo as info de arquivo*/}
             <meter min="0" max="100" value="98.1" high={10} low={90} optimum={50}>98% Used</meter>
             <span>98.1 GB de 100 GB usados</span>
-            {/* adicionar daqui pra baixo um botão para comprar mais armazenamento*/}
+            <a className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">add</i></a>
         </div>
     );
 }
