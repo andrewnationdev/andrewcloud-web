0445dd026c96030e859055dde9c6f874d3aaa8b2
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 22:27:01 -0300

    alterando cores do botao

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -3,11 +3,11 @@ import '../../styles/main.css';
 export default function SidebarComponent(){
     return(
         <div className="sidebar-container">
-            <a className="waves-effect waves-light btn"><i className="material-icons left">cloud</i>button</a>
+            <a className="waves-effect waves-light btn light-blue accent-3"><i className="material-icons left">Enviar arquivo</i>button</a>
             {/* Adicionar daqui pra baixo as info de arquivo*/}
             <meter min="0" max="100" value="98.1" high={10} low={90} optimum={50}>98% Used</meter>
             <span>98.1 GB de 100 GB usados</span>
-            <a className="waves-effect waves-light btn"><i className="material-icons left">cloud</i>button</a>
+            <a className="waves-effect waves-light btn light-blue accent-4"><i className="material-icons left">Comprar mais armazenamento</i>button</a>
         </div>
     );
 }
