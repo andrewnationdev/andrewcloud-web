b814c7916b3c0487398ccdefe62b8854beac7523
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 23:05:07 -0300

    adicionando informaçoes de arquivos

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -10,7 +10,12 @@ export default function SidebarComponent() {
                 Enviar arquivo
             </a>
             <input type="file" id="avatar" hidden name="avatar" accept="image/png, image/jpeg" />
-            {/* Adicionar daqui pra baixo as info de arquivo*/}
+            
+             </span><strong>Tamanho:</strong>11MB<span>
+             </span><strong>Tipo:</strong> PNG<span>
+             </span><strong>Modificado em:</strong> 25/08/2023 às 19:19<span>
+             </span><strong>Acesso:</strong> Privado<span>
+            
             <meter min="0" max="100" value="98.1" high={10} low={90} optimum={50}>98% Used</meter>
             <span>98.1 GB de 100 GB usados</span>
             <a className="waves-effect waves-light btn light-blue accent-4">
