1cdf8125e41dc2d0a4e92ee942e97d72275faf3f
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 22:22:10 -0300

    alterando card e botões

--- src/components/FileArea/FileCard.component.tsx
+++ src/components/FileArea/FileCard.component.tsx
@@ -1,8 +1,10 @@
 export default function FileCardComponent() {
     return (
         <div className="row">
-            <div className="col s12 m6">
-                <div className="card">
+            <div className="col">
+                <div className="card" style={{
+                    width: "200px"
+                }}>
                     <div className="card-image">
                         <img src="images/sample-1.jpg" />
                         <a className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">more_horiz</i></a>
