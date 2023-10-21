04e1f31335b0b35ca1129fbf426d420474e8c94f
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-21 20:32:46 -0300

    adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/Home.page';
+import LoginScreen from './pages/Login.page';
 import { useQuery } from 'react-query';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import Router from 'react-easy-router';
 
 function App() {
-  const queryClient = new QueryClient();
+  const { data, isLoading, isError, refetch } = useQuery('global-query', () => { });
 
-  const { data, isLoading, isError, refetch } = useQuery('global-query', () => {});
-  
+  const routes = [
+    {
+      path: "/",
+      navigate: "/login"
+    },
+    {
+      path: "/login",
+      element: <LoginScreen/>
+    },
+    {
+      path: "/files",
+      element: <HomePage/>
+    }
+  ]
+
+  const routes = [...routes];
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <HomePage/>
-      </div>
-    </QueryClientProvider>
+    <div className="App">
+      <Router routes={routes} />
+    </div>
   );
 }
 
