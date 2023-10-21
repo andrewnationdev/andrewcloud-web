4473591afaff9cab0751abb6d613a238a235d899
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 22:44:51 -0300

    adicionando bobão arquivo

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -3,10 +3,13 @@ import '../../styles/main.css';
 export default function SidebarComponent() {
     return (
         <div className="sidebar-container">
-            <a className="waves-effect waves-light btn light-blue accent-3">
+            <a className="waves-effect waves-light btn light-blue accent-3"
+                onClick={() => document.getElementById('avatar')?.click()}
+            >
                 <i className="material-icons left">archive</i>
                 Enviar arquivo
             </a>
+            <input type="file" id="avatar" hidden name="avatar" accept="image/png, image/jpeg" />
             {/* Adicionar daqui pra baixo as info de arquivo*/}
             <meter min="0" max="100" value="98.1" high={10} low={90} optimum={50}>98% Used</meter>
             <span>98.1 GB de 100 GB usados</span>
