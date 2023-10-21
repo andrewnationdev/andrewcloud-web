a6df8bbb988b4c22bb65b693520d1b97eadd2ee3
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-21 20:11:09 -0300

    versão react-query - ajustando no package json

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/Home.page';
+import { useQuery } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function App() {
+  const queryClient = new QueryClient();
+  
+  const { data, isLoading, isError, refetch } = useQuery('global-query', fetchData);
+  
   return (
-    <div className="App">
-      <HomePage/>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <HomePage/>
+      </div>
+    </QueryClientProvider>
   );
 }
 
