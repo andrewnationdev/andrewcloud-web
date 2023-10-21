2aaef3b65833d6b13dbc768892621faf389c6d54
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 22:23:11 -0300

    adicionando classname

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -3,11 +3,11 @@ import '../../styles/main.css';
 export default function SidebarComponent(){
     return(
         <div className="sidebar-container">
-            <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+            <a className="waves-effect waves-light btn"><i className="material-icons left">cloud</i>button</a>
             {/* Adicionar daqui pra baixo as info de arquivo*/}
             <meter min="0" max="100" value="98.1" high={10} low={90} optimum={50}>98% Used</meter>
             <span>98.1 GB de 100 GB usados</span>
-            <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+            <a className="waves-effect waves-light btn"><i className="material-icons left">cloud</i>button</a>
         </div>
     );
 }
