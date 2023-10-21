a6d85362df705e6e08d6dea71ab5bea79e84f823
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-20 22:02:28 -0300

    adicionando comentários

--- src/components/Sidebar/Sidebar.component.tsx
+++ src/components/Sidebar/Sidebar.component.tsx
@@ -3,10 +3,11 @@ import '../../styles/main.css';
 export default function SidebarComponent(){
     return(
         <div className="sidebar-container">
-            <meter min="0" max="100" value="98.1" high={10} low={90} optimum={90}>98% Used</meter>
+            {/* adicionar daqui pra baixo o botão de upload */}
+            {/* Adicionar daqui pra baixo as info de arquivo*/}
+            <meter min="0" max="100" value="98.1" high={10} low={90} optimum={50}>98% Used</meter>
             <span>98.1 GB de 100 GB usados</span>
-            <meter min="0" max="100" value="3.11" high={10} low={90} optimum={90}>3% Used</meter>
-            <span>3.11 GB de 100 GB usados</span>
+            {/* adicionar daqui pra baixo um botão para comprar mais armazenamento*/}
         </div>
     );
 }
