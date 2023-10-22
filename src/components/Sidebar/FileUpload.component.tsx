f9ffa91de9e75ee2abfbe646c957cc9cf1fda71f
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-22 09:44:37 -0300

    atualizando

--- src/components/Sidebar/FileUpload.component.tsx
+++ src/components/Sidebar/FileUpload.component.tsx
@@ -10,7 +10,7 @@ export default function FileUploadComponent() {
         const totalSteps = 20; 
         const uploadInterval = 500; 
 
-        const fileSize = file!.size; 
+        const fileSize = file!.size!; 
 
         setUploadSize(fileSize);
 
@@ -34,7 +34,7 @@ export default function FileUploadComponent() {
 
     useEffect(() => {
         console.log(file);
-        simulateUpload();
+        if(file) simulateUpload();
     }, [file])
 
     return (
