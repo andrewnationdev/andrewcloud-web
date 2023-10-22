0b7ca8e279e0324a21274172feba3ee3ba431c51
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-21 21:32:32 -0300

    form

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -20,13 +20,13 @@ export default function LoginScreen() {
                 <div className="row">
                     <div className="input-field col s12">
                         <input id="email" type="email" className="validate" />
-                        <label for="email">Email</label>
+                        <label htmlFor="email">Email</label>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-field col s12">
                         <input id="password" type="password" className="validate" />
-                        <label for="password">Password</label>
+                        <label htmlFor="password">Password</label>
                     </div>
                 </div>
                 <div className="row">
@@ -34,7 +34,7 @@ export default function LoginScreen() {
                         This is an inline input field:
                         <div className="input-field inline">
                             <input id="email_inline" type="email" className="validate" />
-                            <label for="email_inline">Email</label>
+                            <label htmlFfor="email_inline">Email</label>
                             <span className="helper-text" data-error="wrong" data-success="right">Helper text</span>
                         </div>
                     </div>
