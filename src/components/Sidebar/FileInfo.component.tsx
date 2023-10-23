793a1fd21e595ff75f97262dfd7ffcc0f85ac813
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-23 12:06:34 -0300

    render condicional file info

--- src/components/Sidebar/FileInfo.component.tsx
+++ src/components/Sidebar/FileInfo.component.tsx
@@ -1,24 +1,27 @@
 import useFileStore from '../../modules/state';
 import FileActionButtonsComponent from './FileActionButtons.component'
+import {bytesToMegabytes} from '../../utils/convert';
 
 export default function FileInfoComponent() {
+    const { data, updateData } = useFileStore();
+
     return <div style={{
         display: "flex",
         flexDirection: "column"
     }}>
-        <div style={{
+        {data?.selectedFile && <div style={{
             display: "flex",
             flexDirection: "column",
             alignItems: 'flex-start',
             textAlign: "left"
         }}>
             <strong><u>Informações do arquivo:</u></strong>
-            <span><strong>Nome:</strong> nome.txt</span>
-            <span><strong>Tamanho:</strong>11MB</span>
-            <span><strong>Tipo:</strong> PNG</span>
-            <span><strong>Modificado em:</strong> 25/08/2023 às 19:19</span>
-            <span><strong>Acesso:</strong> Privado</span>
+            <span><strong>Nome:</strong>{data?.selectedFile?.name}</span>
+            <span><strong>Tamanho:</strong>{bytesToMegabytes(data?.selectedFile?.size)}</span>
+            <span><strong>Tipo:</strong>{data?.selectedFile?.type}</span>
+            <span><strong>Modificado em:</strong>{data?.selectedFile?.lastModified}</span>
+            <span><strong>Acesso:</strong>privado</span>
             <FileActionButtonsComponent />
-        </div>
+        </div>}
     </div>
 }
