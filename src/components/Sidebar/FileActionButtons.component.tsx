d835dddd65ba3f1effc28a2d418512deb1728310
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-23 19:49:03 -0300

    correção

--- src/components/Sidebar/FileActionButtons.component.tsx
+++ src/components/Sidebar/FileActionButtons.component.tsx
@@ -1,7 +1,7 @@
 export default function FileActionButtonsComponent(){
     return <>
-    <a className="btn-floating btn-large waves-effect waves-blue darken-1"><i className="material-icons">add</i></a>
-    <a className="btn-floating btn-large waves-effect waves-blue darken-2"><i className="material-icons">add</i></a>
-    <a classname="btn-floating btn-large waves-effect waves-blue darken-3"><i className="material-icons">add</i></a>
-    <a className="btn-floating btn-large waves-effect blue darken-4"><i className="material-icons">add</i></a></>
+    <a className="btn-floating btn-large waves-effect waves-blue darken-1"><i className="material-icons">share</i></a>
+    <a className="btn-floating btn-large waves-effect waves-blue darken-2"><i className="material-icons">remove_red_eye</i></a>
+    <a className="btn-floating btn-large waves-effect waves-blue darken-3"><i className="material-icons">file_download</i></a>
+    <a className="btn-floating btn-large waves-effect blue darken-4"><i className="material-icons">remove</i></a></>
 }
