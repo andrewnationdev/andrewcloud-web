6d2efdfcf4008d0f312dfb5bb7ee7cc37b871485
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-23 11:46:56 -0300

    atualizando

--- src/components/Sidebar/Storage.component.tsx
+++ src/components/Sidebar/Storage.component.tsx
@@ -1,5 +1,5 @@
 import useFileStore from '../../modules/state'
-import {byteToGigabytes} from '../../utils/convert';
+import {bytesToGigabytes} from '../../utils/convert';
 
 export default function StorageComponent() {
     const {data} = useFileStore();
@@ -11,13 +11,13 @@ export default function StorageComponent() {
         <meter 
             min="0" 
             max="100" 
-            value={byteToGigabytes(data?.storageQuota)}
+            value={bytesToGigabytes(data?.storageQuota)}
             high={10} 
             low={90} 
             optimum={50}
         >98% Used</meter>
         <span>
-            {byteToGigabytes(data?.storageQuota).toFixed(2)} de 100 GB usados
+            {bytesToGigabytes(data?.storageQuota).toFixed(2)} GB de 100 GB usados
         </span>
         </div>
 }
