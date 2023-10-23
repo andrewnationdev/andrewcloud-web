cfcc4b59a257387be370769759b39b55d47acdf8
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-23 11:52:48 -0300

    correção tipagem

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -1,7 +1,7 @@
-export function bytesToMegabytes(bytes) {
+export function bytesToMegabytes(bytes: number) {
     return bytes / 1048576.0;
 }
   
-export function bytesToGigabytes(bytes) {
+export function bytesToGigabytes(bytes: number) {
     return bytes / 1073741824.0;
 }
