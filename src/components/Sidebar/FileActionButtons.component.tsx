4fc8ed0d102d166edc64e59a072fccd521583b38
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-23 12:16:54 -0300

    adicionando botao e alterando cores do botao

--- src/components/Sidebar/FileActionButtons.component.tsx
+++ src/components/Sidebar/FileActionButtons.component.tsx
@@ -1,6 +1,7 @@
 export default function FileActionButtonsComponent(){
-    return <div>
-
-        
-    </div>
+    return <>
+    <a className="btn-floating btn-large waves-effect waves-blue darken-1"><i className="material-icons">add</i></a>
+    <a className="btn-floating btn-large waves-effect waves-blue darken-2"><i className="material-icons">add</i></a>
+    <a classname="btn-floating btn-large waves-effect waves-blue darken-3"><i className="material-icons">add</i></a>
+    <a className="btn-floating btn-large waves-effect blue darken-4"><i className="material-icons">add</i></a></>
 }
