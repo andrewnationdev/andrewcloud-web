46806c65978f02c87afeb2f524cec291c657d412
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-23 23:07:01 -0300

    diminuindo tamanho dos botões

--- src/components/Sidebar/FileActionButtons.component.tsx
+++ src/components/Sidebar/FileActionButtons.component.tsx
@@ -1,7 +1,22 @@
 export default function FileActionButtonsComponent(){
-    return <>
-    <a className="btn-floating btn-large waves-effect waves-blue darken-1"><i className="material-icons">share</i></a>
-    <a className="btn-floating btn-large waves-effect waves-blue darken-2"><i className="material-icons">remove_red_eye</i></a>
-    <a className="btn-floating btn-large waves-effect waves-blue darken-3"><i className="material-icons">file_download</i></a>
-    <a className="btn-floating btn-large waves-effect blue darken-4"><i className="material-icons">remove</i></a></>
+    return <div style={{
+        display: "flex",
+    }}>
+        <a 
+            className="btn-floating waves-effect waves-blue darken-1">
+            <i className="material-icons">share</i>
+        </a>
+        <a 
+            className="btn-floating waves-effect waves-blue darken-2">
+            <i className="material-icons">remove_red_eye</i>
+        </a>
+        <a 
+            className="btn-floating waves-effect waves-blue darken-3">
+            <i className="material-icons">file_download</i>
+        </a>
+        <a 
+            className="btn-floating waves-effect blue darken-4">
+            <i className="material-icons">remove</i>
+        </a>
+    </div>
 }
