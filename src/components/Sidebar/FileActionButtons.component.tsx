0c798ddc94259d315731c5eff903b529f635fd9c
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-24 12:24:43 -0300

    tipagem

--- src/components/Sidebar/FileActionButtons.component.tsx
+++ src/components/Sidebar/FileActionButtons.component.tsx
@@ -25,7 +25,7 @@ export default function FileActionButtonsComponent(){
 
     const handlePreviewFile = () => {
         try {
-            const url = URL.createObjectURL(data.selectedFile!.blob!);
+            const url = URL.createObjectURL(data.selectedFile!.blob! as Blob);
             window.open(url);
         } catch(err) {
             notify("Não é possível pré-visualizar ou baixar o arquivo.")
