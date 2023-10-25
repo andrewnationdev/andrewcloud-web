108d72aff2865bb8b9ea56fab0ff04bb7965514c
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-25 17:10:27 -0300

    alterando arquivo de sem componentes

--- src/components/FileArea/NoFiles.component.tsx
+++ src/components/FileArea/NoFiles.component.tsx
@@ -1,12 +1,21 @@
 export default function NoFilesComponent() {
     const imgsrc = "https://cdn4.iconfinder.com/data/icons/treasure-hunter-2/500/box_empty_treasure_ancient_wealth_old_pirate_wooden_spider_web-256.png"
-    
+
     return (
-        <div>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: "100%",
+            justifyContent: "center",
+            gap: "1rem"
+        }}>
             <img
                 src={imgsrc}
                 alt="Not found error" />
-            <span>Nenhum arquivo enviado. Clique em <u>ENVIAR.</u></span>
+            <span style={{
+                
+            }}>Nenhum arquivo enviado. Clique em <u>ENVIAR.</u></span>
         </div>
     )
 }
