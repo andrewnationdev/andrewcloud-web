97d736523e85f8237f7b556743b3bc826ff6d896
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-24 22:25:53 -0300

    correçao

--- src/modules/state.ts
+++ src/modules/state.ts
@@ -1,6 +1,6 @@
 import create from 'zustand';
-import {IFile} from '../types/file';
-import {persist} from 'zustand/middleware'
+import { IFile } from '../types/file';
+import { persist } from 'zustand/middleware';
 
 interface IStoreActions {
   updateData: (newData: IAndrewCloud) => void;
@@ -11,22 +11,22 @@ interface IAndrewCloud {
   avatar: string;
   files: IFile[];
   storageQuota: number;
-  selectedFile: IFile | null; 
+  selectedFile: IFile | null;
 }
 
-const useFileStore = create<{data: IAndrewCloud } & IStoreActions>(persist((set) => ({
+const useFileStore = create<{ data: IAndrewCloud } & IStoreActions>(
+  persist((set) => ({
     data: {
-        user: '',
-        avatar: 'https://th.bing.com/th/id/OIP._Ub60e6muH_f6fAyfyZW8AHaEK?pid=ImgDet&rs=1',
-        files: [],
-        storageQuota: 0,
-        selectedFile: null
+      user: '',
+      avatar: 'https://th.bing.com/th/id/OIP._Ub60e6muH_f6fAyfyZW8AHaEK?pid=ImgDet&rs=1',
+      files: [],
+      storageQuota: 0,
+      selectedFile: null,
     },
-    updateData: (newData) => {
-        set((s) => ({ data: { ...s.data, ...newData } }));
-      },      
-})), {
-  name: "andrew-cloud-local"
-});
+  })),
+  {
+    name: "andrew-cloud-local",
+  }
+);
 
 export default useFileStore;
