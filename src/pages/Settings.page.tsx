17e1c8c2f2e3a7f0ec523497fd0a1f8a6b15a47d
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-25 16:52:59 -0300

    adicionando header no configurações

--- src/pages/Settings.page.tsx
+++ src/pages/Settings.page.tsx
@@ -1,10 +1,13 @@
+import HeaderComponent from '../components/Header/Header.component';
+
 export default function SettingsPage(){
     //Nome, senha, e-mail, avatar...
 
     return(
         <>
+            <HeaderComponent/>
             <h2>Configurações</h2>
-            
+
         </>
     )
 }
