673584ab26d9d9e4828a2dd7f97a1e54b0baf437
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-24 22:35:36 -0300

    store

--- src/modules/state.ts
+++ src/modules/state.ts
@@ -14,19 +14,24 @@ interface IAndrewCloud {
   selectedFile: IFile | null;
 }
 
-const useFileStore = create<{ data: IAndrewCloud } & IStoreActions>(
-  persist((set) => ({
-    data: {
+const useFileStore = create<IAndrewCloud & IStoreActions>(
+  persist(
+    (set) => ({
       user: '',
       avatar: 'https://th.bing.com/th/id/OIP._Ub60e6muH_f6fAyfyZW8AHaEK?pid=ImgDet&rs=1',
       files: [],
       storageQuota: 0,
       selectedFile: null,
-    },
-  }),
-  {
-    name: "andrew-cloud-local",
-  }
+    }),
+    {
+      name: 'andrew-cloud-local',
+      getStorage: () => localStorage.getItem('yourLocalStorageKey'),
+      onRehydrateStorage: (value) => {
+        if (value) {
+          set(value); // Update the entire state with the retrieved value
+        }
+      },
+    }
   )
 );
 
