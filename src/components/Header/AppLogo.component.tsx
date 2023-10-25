8c1169b89feac8983548d6220a84b0438e22b4b6
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2023-10-25 16:38:11 -0300

    contêiner ajustes de logo

--- src/components/Header/AppLogo.component.tsx
+++ src/components/Header/AppLogo.component.tsx
@@ -1,8 +1,11 @@
 import '../../styles/header.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function AppLogoComponent(){
+    const navigate = useNavigate();
+    
     return(
-        <div className="app-logo-container">
+        <div className="app-logo-container" onClick={()=> navigate("/files")}>
             <img className="app-logo" src="/assets/logo.png" alt="AndrewCloudlogo" />
             <span className="app-logo-text">AndrewCloud</span>
         </div>
